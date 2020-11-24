cbcc326606f7b3f3b7c15d724238e93d867d4d2f
Miriam <miriam.haenle@gmail.com>
2020-11-24 18:12:30 +0100

    Add some styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { useEffect, useState } from 'react';
-import getCharacters from './services/getCharacters';
-import Characters from './components/Characters';
+import { useEffect, useState } from 'react'
+import getCharacters from './services/getCharacters'
+import Characters from './components/Characters'
+import styled from 'styled-components/macro'
 
 function App() {
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     getCharacters()
       .then((characters) => setCharacters(characters))
-      .catch((error) => console.error(error.message));
-  }, []);
+      .catch((error) => console.error(error.message))
+  }, [])
 
   return (
-    <>
+    <Container>
       <h1>Characters:</h1>
       <Characters characters={characters} />
-    </>
-  );
+    </Container>
+  )
 }
 
-export default App;
+export default App
+
+const Container = styled.div`
+  background: #17141d;
+  color: ivory;
+  margin: 0;
+  text-align: center;
+
+  h1 {
+    margin: 0;
+    padding: 5px;
+  }
+`
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,9 +1,36 @@
+import styled from 'styled-components/macro'
+
 export default function Characters({ characters }) {
   return (
-    <ul>
+    <Container>
       {characters.map((character) => (
-        <li key={character.name}>{character.name}</li>
+        <li key={character.name}>
+          <div>{character.name}</div>
+          <div>{character.house}</div>
+          <img src={character.image} alt={character.name} />
+        </li>
       ))}
-    </ul>
-  );
+    </Container>
+  )
 }
+
+const Container = styled.ul`
+  color: #fff;
+  display: grid;
+  font-family: sans-serif;
+  gap: 4px;
+  padding: 0;
+
+  li {
+    background: #17141d;
+    box-shadow: -1rem 0 3rem #000;
+    list-style: none;
+    padding: 10px;
+    text-align: center;
+    width: 70%;
+
+    img {
+      width: 50px;
+    }
+  }
+`
